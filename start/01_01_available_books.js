7639f6da79d5edfa71e988e70dd21ff3ef4993bb
DanaKLym <bohdanaklymenko@gmail.com>
2024-01-04 23:51:30 +0200

    first task completed

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,1 +1,45 @@
 // Write your code here
+
+const Book = class Book{
+  constructor (title, author, ISBN, numCopies){
+    this.title = title;
+    this.author = author;
+    this.ISBN = ISBN;
+    this.numCopies = numCopies;
+  }
+
+get availability() {
+  return this.getAvailability();
+  }
+
+
+ getAvailability(){
+  if (this.numCopies === 0){
+    return "out of stock"
+  } else if (this.numCopies < 10) {
+    return "low stock";
+  } else {
+    return "in stock";
+  }
+  };
+
+sell(numCopiesSold = 1) {
+  this.numCopies -= numCopiesSold;
+}
+
+restock(numCopiesStocked = 5) {
+  this.numCopies += numCopiesStocked;
+}
+
+};
+
+
+
+const HarryPotter = new Book("Harry Potter", "Rowling", 1234485, 6);
+console.log(HarryPotter.availability);
+
+HarryPotter.restock(24);
+console.log(HarryPotter.availability);
+
+HarryPotter.sell(18);
+console.log(HarryPotter.availability);
