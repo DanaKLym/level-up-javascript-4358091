3ced32912cef66f139e9a78b549023ab128c21c2
DanaKLym <bohdanaklymenko@gmail.com>
2024-01-12 01:06:27 +0200

    fifth task completed

--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,1 +1,15 @@
 // Write your code here
+
+const username = Symbol("username");
+const password = Symbol("password");
+
+const user = {
+  [username]: "Dana Klym",
+  [password]: 1234568,
+  age: 28
+};
+
+console.log(user.username);
+console.log(user.password);
+
+//NOTE: if we created Symbol() before and would get the object properties in [] and run "console.log" with square-bracketed props, this would return "undefined";
