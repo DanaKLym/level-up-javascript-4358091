55aa16ff35cba62603488e9995a3c3fd75677cc5
DanaKLym <bohdanaklymenko@gmail.com>
2024-01-12 00:43:32 +0200

    fourth task completed

--- start/01_04_technical_books.js
+++ start/01_04_technical_books.js
@@ -31,3 +31,20 @@ class Book {
 }
 
 // Write your code here
+class EditionBook extends Book {
+  constructor (title, author, ISBN, numCopies, edition) {
+    super(title, author, ISBN, numCopies);
+    this.edition = edition;
+  }
+
+  getEdition(edition) {
+    return `The current version of "${this.title}" book is ${this.edition}`
+  }
+}
+
+const hungerGames = new EditionBook("Hunger Games", "Suzanne Collins", 5451, 12, "2nd");
+const harryPotter = new Book("Harry Potter", "Rowling", 1234485, 6);
+
+console.log(harryPotter.availability);
+console.log(hungerGames);
+console.log(hungerGames.getEdition());
